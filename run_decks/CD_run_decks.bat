710f2593eda7332845bbcbde6fab01c811699f7d
aijac0 <aidancollinscs@gmail.com>
2025-07-29 09:16:47 -0400

    Removed batch files, some of which reference input files that contain deprecated features

--- run_decks/CD_run_decks.bat
+++
@@ -1,3 +0,0 @@
-./screamer64 CD_cell_.5_1_.5_20ohm.txt
-./screamer64 CD_cell_1_1.3_1_20ohm.txt
-./screamer64 CD_cell_1_1_1_20ohm.txt
--- run_decks/Saturn_old.bat
+++
@@ -1,4 +0,0 @@
-./screamer64 Saturn_old_rev1_200.txt
-./screamer64 Saturn_old_rev1_300.txt
-./screamer64 Saturn_old_rev1_400.txt
-./screamer64 Saturn_old_rev1_500.txt
